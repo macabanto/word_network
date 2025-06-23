b54b964c08d788f042ebe740e0aa201cb9761c05
macabanto <antmacapple@gmail.com>
2025-06-23 15:15:27 +1000

    final push for local scraper; beginning transition to cloud-based processing

--- scripts/collins_scrape.py
+++ scripts/collins_scrape.py
@@ -79,7 +79,7 @@ def setup_selenium():
 #    chrome_options.add_argument("--no-sandbox")
 #    chrome_options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome()
     return driver
 
 # Fetch HTML content using Selenium
